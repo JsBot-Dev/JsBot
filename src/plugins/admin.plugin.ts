b28358100b8d704a8f53e4a31622f59144cccb11
J-SLY <2029872426@qq.com>
2026-08-19 19:04:20 +0800

    Feat(AdminPlugin): 新增 /unban 指令

--- src/plugins/admin.plugin.ts
+++ src/plugins/admin.plugin.ts
@@ -1,6 +1,6 @@
-import { fromCQString, isGroupMessageEvent, OneBotMessageEvent, SnowLumaEventContext, SnowLumaWebSocketClient, text} from "@snowluma/sdk";
+import { fromCQString, isGroupMessageEvent, SnowLumaWebSocketClient, text} from "@snowluma/sdk";
 import { EnableGroup, DefaultBanDuration } from "./config/admin.plugin.config";
-import { BotRole, RefuseCommand } from "../utils/tools";
+import { RefuseCommand } from "../utils/tools";
 
 export default class AdminPlugin{
     public register(
@@ -72,6 +72,62 @@ export default class AdminPlugin{
             });
 
             if(ban.status=='ok'){
+                ctx.reply(
+                    text("指令执行成功").reply(event.message_id)
+                );
+            }else{
+                ctx.reply(
+                    text("指令执行失败，请重试").reply(event.message_id)
+                );
+            }
+        });
+        bot.command('unban',async(event,ctx,match)=>{
+            if(!isGroupMessageEvent(event))return;
+            if(!this.isEnable(event.group_id))return;
+
+            const groupId = event.group_id;
+            const userId = event.user_id;
+
+            if(!await this.isAdmin(groupId,userId,bot)){
+                RefuseCommand(ctx,event);
+                console.log(`${userId} 权限不足，拒绝执行 /unban`);
+                return;
+            }
+
+            const args = match.args;
+            if(args.length<1){
+                ctx.reply(
+                    text("参数检定不合法，用法").br().br()
+                    .text("/unban <目标用户>")
+                    .reply(event.message_id)
+                )
+                console.log("参数数量不足")
+                return;
+            }
+
+            const userNumber = this.AtToNumber(args[0]);
+            if(userNumber == null){
+                ctx.reply(
+                    text("目标用户不存在").reply(event.message_id)
+                )
+                console.log("目标用户不存在");
+                return;
+            }
+            if(userNumber==event.self_id||userNumber==userId){
+                ctx.reply(
+                    text("解禁目标不合法").reply(event.message_id)
+                )
+                console.log("目标不合法");
+                return;
+            }
+
+            const unban = await bot.rawResponse('set_group_ban',{
+                group_id:groupId,
+                user_id:userNumber,
+                duration:0
+            });
+
+            if(unban.status=='ok'){
                 ctx.reply(
                     text("指令执行成功").reply(event.message_id)
                 );
