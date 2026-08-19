e2231c4b14795c40c06bbef9059896a028e47e5e
J-SLY <2029872426@qq.com>
2026-08-19 14:37:40 +0800

    Fix: 因添加鉴权而移除了Echo指令的前缀

--- src/plugins/echo.plugin.ts
+++ src/plugins/echo.plugin.ts
@@ -5,7 +5,7 @@ const prefix = "Special Echo Command: ";
 export default function echoPlugin(bot:SnowLumaWebSocketClient){
     bot.command('test.raw',async(event,ctx,match)=>{
         if(BotRole(event.user_id)!='User'){
-            await ctx.reply(text(prefix+match.rest));
+            await ctx.reply(text(match.rest));
             console.log(`管理员 ${event.user_id} 使用了指令'test.raw'，内容为 ${match.rest}`);
         }else{
             await ctx.reply(text("权限不足").face(79))
@@ -13,7 +13,7 @@ export default function echoPlugin(bot:SnowLumaWebSocketClient){
     });
     bot.command('test',async(event,ctx,match)=>{
         if(BotRole(event.user_id)!='User'){
-            await ctx.reply(prefix+fromCQString(match.rest));
+            await ctx.reply(fromCQString(match.rest));
             console.log(`管理员 ${event.user_id} 使用了指令'test'，内容为 ${match.rest}`);
         }else{
             await ctx.reply(text("权限不足").face(79))
